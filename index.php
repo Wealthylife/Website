6627c6c8649d2de470c20053ccfbbb92cb1e2155
WealthFactory <gene.reese.mail@gmail.com>
2014-01-29 14:49:46 -0500

    Index file for website content
    Content for the corporate site

---
+++ index.php
@@ -0,0 +1,370 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<title>Wealth Factory</title>
+	<meta name="description" content="wealthy life" />
+	<meta name="keywords" content="wealthy life" />
+	<meta name="author" content="Abhishek Goel" />
+
+	<link rel="shortcut icon" type="image/x-icon" href="images/icon.png"> 
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/component.css" />
+	<link rel="stylesheet" type="text/css" href="css/pricing.css" />
+
+	<script src="js/modernizr.custom.js"></script>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/modernizr.bootstrap.min.js"></script>
+</head>
+
+
+<body>
+
+<div class="container">	
+
+<div id="bl-main" class="bl-main">
+
+
+	<section>
+		<div class="bl-box">
+			<h2 class="bl-icon bl-icon-about">About Us</h2>
+		</div>
+		<div class="bl-content">
+			<br><h2>About Us</h2><br>
+
+			<div class="row">
+				<div class="col-lg-10">
+					<ul>
+						<li class='lead' align="justify">The Wealth Factory, Inc. is the developer of WealthyLife©, an adaptive learning platform that teaches people how to make better finacial decisions. 
+							WealthyLife was designed to improve financial literacy by 30% and is structured according to the Common Core Standards of the Business and Finance Technology Personal Finance Education Framework.</li>
+						<li class='lead' align="justify">The 20 virtual stages are structured as mini-games to develop procedural skills and conceptual understanding of specific topics. Students can maintain tailored learning experiences via independent working times supported by individualized performance tracking, allowing teachers to simultaneously teach a big picture subject while addressing unique learning needs.</li>
+						<li class='lead' align="justify">All these platforms and tools will come with the support and knowledge our customers have come to rely on. We ensure that customer’s 
+							questions are resolved timely and effectively from our friendly and talented team.</li>
+					</ul>
+				</div>
+			</div>
+			
+		</div>
+		<span class="bl-icon bl-icon-close"></span>
+	</section>
+	
+
+	<section id="bl-work-section">
+		<div class="bl-box">
+			<h2 class="bl-icon bl-icon-services">Products & Applications</h2>
+		</div>
+		<div class="bl-content">
+			<h2>Products & Applications we provide...</h2>
+			<ul id="bl-work-items">
+
+				<!-- div -->
+				<li data-panel="panel-1">
+					<a href="#"><img src="images/1.jpg" /></a>
+					<h3>Profile View</h3>
+					<p align="justify">Every teacher and student will have his/her login username and password. Any student/ teacher will be 
+						required to log in into system to accesss to all the online platforms. Once they log in, they will be able to see 
+						their profile details.</p>
+						<a href="#">Read More...</a>
+				</li>
+				<li data-panel="panel-2">
+					<a href="#"><img src="images/2.jpg" /></a>
+					<h3>Search Platform</h3>
+					<p align="justify">This is a platform where students/ teachers can find complete information of any Student 
+						like Name, Class, Section, Roll Number etc. This is a search platform i.e. if you enter name of a student 
+						you will get complete info of...</p>
+						<a href="#">Read More...</a>
+				</li>
+				<li data-panel="panel-3">
+					<a href="#"><img src="images/3.jpg" /></a>
+					<h3>Feedback Platform</h3>
+					<p align="justify">In this platform a student can submit online feedback for the teachers teaching that student. The feedback will 
+						and should be anonymous. So, we can conduct monthly/ half-yearly or yearly feedbacks. Teacher will have...</p>
+						<a href="#">Read More...</a>
+				</li>
+			</ul>
+		</div>
+		<span class="bl-icon bl-icon-close"></span>
+	</section>
+
+
+
+	<section>
+		<div class="bl-box">
+			<h2 class="bl-icon bl-icon-why">Why Us</h2>
+		</div>
+		<div class="bl-content">
+			<h2>Reasons to choose us...</h2><br>
+
+			<div class="row"><div class="col-lg-10">
+				<ul>
+					<li class="lead"><article>
+						<h5>Free Demo & Money Back Gurantee</h5>
+						<p align="justify">We are absolutely sure that you will be satisfied with our service as well as product that we offer our unconditional 
+							money back guarantee. Feel free to try out our product and take a ride with us without any future risk!</p>
+					</article></li>
+
+					<li class="lead"><article>
+						<h5>Cost Effective Pricing</h5>
+						<p align="justify">We provide our services at the lowest cost possible by choosing cost effective tools and technologies. We strive to keep 
+							our operating costs low so we can deliver quality web application services at a very reasonable prices.</p>
+					</article></li>
+
+					<li class="lead"><article>
+						<h5>Customer Care Program</h5>
+						<p align="justify">Our commitment to customer service is an important and distinguishing characteristic of our company. Our primary goal is 
+							sustaining long-term relationships with our clients. Our dedicated team of employees provides prompt and reliable services and 
+							guarantee you solution to your problem within 24 hours.</p>
+					</article></li>
+
+					<li class="lead"><article>
+						<h5>Reliability</h5>
+						<p align="justify">We are using high-end servers for increased up-time and stability. Server up-time is a priority and we monitor our 
+							servers 24 hours a day, 365 days a year. Our technicians respond to problems proactively to keep your site running with the least 
+							amount of disruption.</p>
+					</article></li>
+
+					<li class="lead"><article>
+						<h5>Security & Backups</h5>
+						<p align="justify">Our other main goal in this field is security; you can trust that your data is in good hands with us. Our frequent 
+							backups and skilled security experts are the guarantee that neither malicious code, nor harmful attacker makes any damage to 
+							your sensitive data.</p>
+					</article></li>
+
+					<li class="lead"><article>
+						<h5>Fast</h5>
+						<p align="justify">Light speed network connectivity to numerous major Internet backbones for faster response times. We regularly review 
+							our network performance and plan upgrades  to deliver the fastest bandwidth speeds to our customers.</p>
+					</article></li>
+
+					<li class="lead"><article>
+						<h5>Innovation</h5>
+						<p align="justify">We are turned on by new ideas and new technologies. We constantly develop our platform and services as per latest web 
+							technologies and trends and also upgrades it whenever necessary.</p>
+					</article></li>
+				</ul>
+			</div></div>
+			
+		</div>
+		<span class="bl-icon bl-icon-close"></span>
+	</section>
+
+
+	<section>
+		<div class="bl-box">
+			<h2 class="bl-icon bl-icon-pricing">Pricing & Plans</h2>
+		</div>
+		<div class="bl-content">
+			<h2>Pricing & Plans...</h2><br>
+
+			<div class="container">
+
+		        <div class="row flat">
+		            <div class="col-lg-4 col-md-4 col-xs-8">
+		                <ul class="plan plan1">
+		                    <li class="plan-name text-primary">Basic</li>
+		                    <li class="plan-price"><strong><s>&nbsp; $2 </s>&nbsp;&nbsp; $1</strong>  / student / year</li>
+		                    <li><strong>Online Search Platform</strong></li>
+		                    <li><strong>Online Feedback Platform</strong></li>
+		                    <li><strong>Free Web Hosting Service</strong></li>
+		                    <li><strong>Phone and Email</strong> Support ( 24x7 )</li>
+		                    <li><strong><small>( Promotional discounts valid till 30th February )</small></strong></li>
+		                    <li><p class='text-danger'><strong>(for schools with students < 1500)</strong></p></li>
+		                </ul>
+		            </div>
+
+		            <div class="col-lg-4 col-md-4 col-xs-8">
+		                <ul class="plan plan2">
+		                    <li class="plan-name">Standard</li>
+		                    <li class="plan-price"><strong><s>&nbsp; $2 </s>&nbsp;&nbsp; $0.85</strong>  / student / year</li>
+		                    <li><strong>Online Search Platform</strong></li>
+		                    <li><strong>Online Feedback Platform</strong></li>
+		                    <li><strong>Free Web Hosting Service</strong></li>
+		                    <li><strong>Phone and Email</strong> Support ( 24x7 )</li>
+		                    <li><strong><small>( Promotional Discounts Valid till 30th February )</small></strong></li>
+		                    <li><p class='text-danger'><strong>(for schools with 1500 < students < 3000 )</strong></p></li>
+		                </ul>
+		            </div>
+
+		            <div class="col-lg-4 col-md-4 col-xs-8">
+		                <ul class="plan plan4">
+		                    <li class="plan-name">Mighty</li>
+		                    <li class="plan-price"><strong><s>&nbsp; $2 </s>&nbsp;&nbsp; $0.70</strong>  / student / year</li>
+		                    <li><strong>Online Search Platform</strong></li>
+		                    <li><strong>Online Feedback Platform</strong></li>
+		                    <li><strong>Free Web Hosting Service</strong></li>
+		                    <li><strong>Phone and Email</strong> Support ( 24x7 )</li>
+		                    <li><strong><small>( Promotional Discounts Valid till 30th February )</small></strong></li>
+		                    <li><p class='text-danger'><strong>(for schools with students > 3000)</strong></p></li>
+		                    
+		                </ul>
+		            </div>
+		        </div>
+
+			</div> <!-- /container -->
+
+		</div>
+		<span class="bl-icon bl-icon-close"></span>
+	</section>
+
+
+	<section>
+		<div class="bl-box">
+			<h2 class="bl-icon bl-icon-demo">Demos</h2>
+		</div>
+		<div class="bl-content">
+			<!-- <h2>Demo Links...</h2> -->
+			<div class="row">
+				<div class="col-lg-2 col-md-3">
+					<p class='lead'><strong>Demo Link : </strong></p>
+					<p><a href="demo" target="_blank">Click Here...</a></p>
+				</div>
+				<div class="col-lg-3 col-md-4">
+					<p class='lead'><strong>Teacher's Credentials : </strong></p>
+					<p>Username - tuser18</p>
+					<p>Password - tpass18</p>
+				</div>
+				<div class="col-lg-3 col-md-4">
+					<p class='lead'><strong>Student's Credentials : </strong></p>
+					<p>Username - user</p>
+					<p>Password - pass</p>
+				</div>
+
+			</div>
+
+		</div>
+		<span class="bl-icon bl-icon-close"></span>
+	</section>
+
+
+	<section>
+		<div class="bl-box">
+			<h2 class="bl-icon bl-icon-contact">Contact</h2>
+		</div>
+		<div class="bl-content">
+			<h2>Get in touch...</h2> <br><br><br>
+			
+			<div class="row">
+
+				<div class="col-lg-6">
+					<form class="form-horizontal" role="form" method="POST" action=''>
+					    <div class="form-group">
+					        <label for="name" class="col-sm-2 control-label">Name*</label>
+					        <div class="col-sm-10">
+					            <input type="text" name="name" class="form-control" id="name" placeholder="Enter Your Name..." required>
+					        </div>
+					    </div>
+					    <div class="form-group">
+					        <label for="mail" class="col-sm-2 control-label">Email*</label>
+					        <div class="col-sm-10">
+					            <input type="email" name="email" class="form-control" id="mail" placeholder="Enter Your Email-ID..." required>
+					        </div>
+					    </div>
+					    <div class="form-group">
+					        <label for="contact" class="col-sm-2 control-label">Contact</label>
+					        <div class="col-sm-10">
+					            <input type="number" min="1000000000" max="9999999999" name="contact" class="form-control" id="contact" placeholder="Enter Your 10 digit Mobile Number...">
+					        </div>
+					    </div>
+					    <div class="form-group">
+					        <label for="message" class="col-sm-2 control-label">Message*</label>
+					        <div class="col-sm-10">
+					            <textarea rows="4" maxlength="5000" name="message" class="form-control" id="message" placeholder="Enter Your Message..." required></textarea>
+					        </div>
+					    </div>
+					    <div class="form-group">
+					        <div class="col-sm-offset-2 col-sm-10">
+					            <button type="submit" class="btn btn-default">Submit</button>
+					        </div>
+					    </div>
+					</form>
+				</div>
+
+			</div>
+
+		</div>
+		<span class="bl-icon bl-icon-close"></span>
+	</section>
+
+
+	<!-- Panel items for the works -->
+	<div class="bl-panel-items" id="bl-panel-work-items">
+		<div data-panel="panel-1">
+			<div>
+				<img src="images/1.jpg" />
+				<h3>Profile View</h3>
+				<p align="justify">Every teacher and student will have his/her login username and password. Any student/ teacher will be 
+						required to log in into system to accesss to all the online platforms. Once they log in, they will be able to see 
+						their profile details.</p>
+			</div>
+		</div>
+		<div data-panel="panel-2">
+			<div>
+				<img src="images/2.jpg" />
+				<h3>Search Platform</h3>
+				<p align="justify">This is a platform where students/ teachers can find complete information of any Student 
+						like Name, Class, Section, Roll Number etc. This is a search platform i.e. if you enter name of a student 
+						you will get complete info of all the students with that name. If you enter name and class, you will get 
+						complete info of all students with that name and enrolled in that class. If you enter only class you will 
+						get complete info of all students enrolled in that particular class. In this platform a teacher has different 
+						privileges than students for example a student can only look for - student Name, Class, Section and Roll 
+						Number while a teacher along with all this info will also be able to get the contact details of his parents.</p>
+			</div>
+		</div>
+		<div data-panel="panel-3">
+			<div>
+				<img src="images/3.jpg" />
+				<h3>Feedback Platform</h3>
+				<p align="justify">In this platform a student can submit online feedback for the teachers teaching that student. The feedback will 
+						and should be anonymous. So, we can conduct monthly/ half-yearly or yearly feedbacks. Teacher will have the access 
+						to his/her feedbacks only while principal will have access to the feedbacks of all teachers.</p>
+			</div>
+		</div>
+		<nav>
+			<span class="bl-next-work">&gt; Next </span>
+			<span class="bl-icon bl-icon-close"></span>
+		</nav>
+	</div>
+
+</div> <!-- /bl-main -->
+
+</div><!-- /container -->
+
+<script src="js/boxlayout.js"></script>
+<script>
+
+
+	$(function() {
+		Boxlayout.init();
+	});
+
+	// Prevent backspace triggering
+	$(document).unbind('keydown').bind('keydown', function (event) {
+	    var doPrevent = false;
+	    if (event.keyCode === 8) {
+	        var d = event.srcElement || event.target;
+
+	        // Make sure backspace works for other inputs
+	        if ((d.tagName.toUpperCase() === 'INPUT') || d.tagName.toUpperCase() === 'TEXTAREA' || d.tagName.toUpperCase() === 'SELECT') {
+	            doPrevent = d.readOnly || d.disabled;
+	        }
+	        else {
+	            doPrevent = true;
+	        }
+	    }
+
+	    if (doPrevent) {
+	        event.preventDefault();
+	    }
+	});
+
+
+</script>
+
+
+
+</body>
+
+</html>
